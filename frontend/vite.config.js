0bacdd104dc29bace0992d637d2f46844249e165
ayush-ojha-11 <ayushojha1512@gmail.com>
2025-04-03 00:29:22 +0530

    changes to vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5002",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
